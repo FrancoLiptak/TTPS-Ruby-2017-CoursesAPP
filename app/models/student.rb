14b5613e9713aebec175022eeb0a1bb47168a017
Franco Liptak <francoliptak@gmail.com>
2017-12-13 12:17:27 -0300

    Fix en validaciones

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,7 +7,7 @@ class Student < ApplicationRecord
   validates_format_of :email, :with =>/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates_format_of :last_name, :name, :with =>/\A(?:[-a-z']+|[-a-z']+\s[-a-z']*\s?[-a-z']+)\z/i 
   
-  validates :dni, uniqueness: { scope: [:course_id], message: "the DNI must be unique within the same course" }
+  validates :dni, uniqueness: { scope: [:course_id], message: "the DNI must be unique within the same course" }, numericality: { only_integer: true, greater_than: 0 }
   validates :student_number, uniqueness: { scope: [:course_id], message: "the student number must be unique within the same course" }
 
 end
