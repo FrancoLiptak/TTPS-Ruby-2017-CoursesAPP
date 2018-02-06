ee3b39befc9faf3985499381f011adddfa9b0111
Franco Liptak <francoliptak@gmail.com>
2018-02-06 16:41:33 -0300

    Actualizacion seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,17 +11,6 @@ User.create(email: 'admin@admin.com', password: 123456)
 Course.create year: Time.zone.today.year
 Course.create year: (Time.zone.today.year - 1)
 
-Course.all.each do |course|
-  3.times do |i|
-    EvaluationInstance.create(course: course,
-                              title: "Instance #{i +1}", 
-                              passing_score: rand(40...60), 
-                              top_score: rand(61...100),  
-                              date: Time.at(rand((Time.new(course.year, 1, 1))..(Time.new(course.year, 12, 31))))
-                              )
-  end
-end
-
 20.times do |i|
   course = Course.all.sample
   name = Faker::GameOfThrones.character
@@ -35,11 +24,22 @@ end
                   student_number: "#{ rand(12000..15000) }/#{ rand(1..10) }"
                 )
 end
+
+Course.all.each do |course|
+  3.times do |i|
+    EvaluationInstance.create(course: course,
+                              title: "Instance #{i +1}", 
+                              passing_score: rand(40...60), 
+                              top_score: rand(61...100),  
+                              date: Time.at(rand((Time.new(course.year, 1, 1))..(Time.new(course.year, 12, 31))))
+                              )
+  end
+end
   
 Course.all.each do |c|
   c.evaluation_instances.each do |e|
     c.students.shuffle.first(5).each do |s|
-      e.scores.build(score: rand(40..60).to_i, evaluation_instance: e, student: s )
+      e.scores.build(score: rand(40..60).to_i, student: s )
       e.save
     end
   end
